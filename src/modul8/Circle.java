860be62562b7067e296e85c083c382e65c7430d1
YevheniiKostrych <YevheniiKostrych@users.noreply.github.com>
2024-04-24 13:04:02 +0200

    .

--- src/modul8/Circle.java
+++ src/modul8/Circle.java
@@ -3,5 +3,6 @@ package modul8;
 class Circle extends Shape{
     @Override
     public void getNameOfShape() {
+
         System.out.println("Circle");
     }}
--- src/modul8/Shape.java
+++ src/modul8/Shape.java
@@ -2,7 +2,7 @@ package modul8;
 
  public abstract class Shape {
      public abstract void getNameOfShape();
-  }
+ }
 
 
 
